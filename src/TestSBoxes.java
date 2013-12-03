d4f3018c54099dee69085557b76f9f89c096d349
msschubi <m.schubert@tu-bs.de>
2013-12-03 16:56:28 +0100

    Sboxen auf Bitebene implementiert und getestet

--- src/TestSBoxes.java
+++ src/TestSBoxes.java
@@ -1,6 +1,9 @@
+import java.math.BigInteger;
+
 public class TestSBoxes {
 
-    static final byte[][] Sbox = new byte[][] { { 3, 8, 15, 1, 10, 6, 5, 11, 14, 13, 4, 2, 7, 0, 9, 12 },/* S0: */
+    static final byte[][] Sbox = new byte[][] { 
+    { 3, 8, 15, 1, 10, 6, 5, 11, 14, 13, 4, 2, 7, 0, 9, 12 },/* S0: */
     { 15, 12, 2, 7, 9, 0, 5, 10, 1, 11, 14, 8, 6, 13, 3, 4 },/* S1: */
     { 8, 6, 7, 9, 3, 12, 10, 15, 13, 1, 14, 4, 0, 11, 5, 2 },/* S2: */
     { 0, 15, 11, 8, 12, 9, 6, 3, 13, 1, 2, 4, 10, 7, 5, 14 },/* S3: */
@@ -33,6 +36,42 @@ public class TestSBoxes {
     { 7, 2, 12, 5, 8, 4, 6, 11, 14, 9, 1, 15, 13, 3, 10, 0 },/* S6: */
     { 1, 13, 15, 0, 14, 8, 2, 11, 7, 4, 12, 10, 9, 3, 5, 6 } /* S7: */
     };
+    
+    static final byte[][] SboxInverse = new byte[][] {
+        {13, 3,11, 0,10, 6, 5,12, 1,14, 4, 7,15, 9, 8, 2 },/* InvS0: */
+        { 5, 8, 2,14,15, 6,12, 3,11, 4, 7, 9, 1,13,10, 0 },/* InvS1: */
+        {12, 9,15, 4,11,14, 1, 2, 0, 3, 6,13, 5, 8,10, 7 },/* InvS2: */
+        { 0, 9,10, 7,11,14, 6,13, 3, 5,12, 2, 4, 8,15, 1 },/* InvS3: */
+        { 5, 0, 8, 3,10, 9, 7,14, 2,12,11, 6, 4,15,13, 1 },/* InvS4: */
+        { 8,15, 2, 9, 4, 1,13,14,11, 6, 5, 3, 7,12,10, 0 },/* InvS5: */
+        {15,10, 1,13, 5, 3, 6, 0, 4, 9,14, 7, 2,12, 8,11 },/* InvS6: */
+        { 3, 0, 6,13, 9,14,15, 8, 5,12,11, 7,10, 1, 4, 2 },/* InvS7: */
+        {13, 3,11, 0,10, 6, 5,12, 1,14, 4, 7,15, 9, 8, 2 },/* InvS0: */
+        { 5, 8, 2,14,15, 6,12, 3,11, 4, 7, 9, 1,13,10, 0 },/* InvS1: */
+        {12, 9,15, 4,11,14, 1, 2, 0, 3, 6,13, 5, 8,10, 7 },/* InvS2: */
+        { 0, 9,10, 7,11,14, 6,13, 3, 5,12, 2, 4, 8,15, 1 },/* InvS3: */
+        { 5, 0, 8, 3,10, 9, 7,14, 2,12,11, 6, 4,15,13, 1 },/* InvS4: */
+        { 8,15, 2, 9, 4, 1,13,14,11, 6, 5, 3, 7,12,10, 0 },/* InvS5: */
+        {15,10, 1,13, 5, 3, 6, 0, 4, 9,14, 7, 2,12, 8,11 },/* InvS6: */
+        { 3, 0, 6,13, 9,14,15, 8, 5,12,11, 7,10, 1, 4, 2 },/* InvS7: */
+        {13, 3,11, 0,10, 6, 5,12, 1,14, 4, 7,15, 9, 8, 2 },/* InvS0: */
+        { 5, 8, 2,14,15, 6,12, 3,11, 4, 7, 9, 1,13,10, 0 },/* InvS1: */
+        {12, 9,15, 4,11,14, 1, 2, 0, 3, 6,13, 5, 8,10, 7 },/* InvS2: */
+        { 0, 9,10, 7,11,14, 6,13, 3, 5,12, 2, 4, 8,15, 1 },/* InvS3: */
+        { 5, 0, 8, 3,10, 9, 7,14, 2,12,11, 6, 4,15,13, 1 },/* InvS4: */
+        { 8,15, 2, 9, 4, 1,13,14,11, 6, 5, 3, 7,12,10, 0 },/* InvS5: */
+        {15,10, 1,13, 5, 3, 6, 0, 4, 9,14, 7, 2,12, 8,11 },/* InvS6: */
+        { 3, 0, 6,13, 9,14,15, 8, 5,12,11, 7,10, 1, 4, 2 },/* InvS7: */
+        {13, 3,11, 0,10, 6, 5,12, 1,14, 4, 7,15, 9, 8, 2 },/* InvS0: */
+        { 5, 8, 2,14,15, 6,12, 3,11, 4, 7, 9, 1,13,10, 0 },/* InvS1: */
+        {12, 9,15, 4,11,14, 1, 2, 0, 3, 6,13, 5, 8,10, 7 },/* InvS2: */
+        { 0, 9,10, 7,11,14, 6,13, 3, 5,12, 2, 4, 8,15, 1 },/* InvS3: */
+        { 5, 0, 8, 3,10, 9, 7,14, 2,12,11, 6, 4,15,13, 1 },/* InvS4: */
+        { 8,15, 2, 9, 4, 1,13,14,11, 6, 5, 3, 7,12,10, 0 },/* InvS5: */
+        {15,10, 1,13, 5, 3, 6, 0, 4, 9,14, 7, 2,12, 8,11 },/* InvS6: */
+        { 3, 0, 6,13, 9,14,15, 8, 5,12,11, 7,10, 1, 4, 2 } /* InvS7: */
+    };
+
     
     static final byte[] IPtable = new byte[] {
         0, 32, 64,  96,  1, 33, 65,  97,  2, 34, 66,  98,  3, 35, 67,  99, //16
@@ -82,6 +121,11 @@ public class TestSBoxes {
        return value;
    }
    
+   public static int get4Bits(int pos, int value) {
+       int ones = 15 << pos-3;
+           return (value&ones)>>>pos-3;
+   }
+   
    /*
     * Gibt zurueck ob Bit an abgefragter Stelle gesetzt ist
     */
@@ -113,17 +157,62 @@ public class TestSBoxes {
        return out;
    }
    
+   /*
+    * Konkateniert 4 Bits an eine bestimmte Position unter Zusicherung, dass an der 
+    * Position sich vorher nur 0en befanden;
+    * auf die Bits wird die entsprechende sBox angewendet
+    */
+   public static int sBox(int pos, int concatValue, int bits, int sBoxNr) {
+       return (Sbox[sBoxNr][bits] << (pos-3)) | concatValue;
+   }
+   
+   public static int invSBox(int pos, int concatValue, int bits, int sBoxNr) {
+       return (SboxInverse[sBoxNr][bits] << (pos-3)) | concatValue;
+   }
+   
     public static void main(String[] args)
     {
         
 //        int i = 0xFFFFFFFF;
-        int i = 15;
-        System.out.println(set4Bits(3, i, 10));
+//        int i = 0;
         
-        System.out.println(i);
+        int a = 1<<31;
+        int b = 1<<30;
+        int c = 1<<29;
+        int d = 1<<28;
+        int konkat = a|b|c|d;
+        System.out.println("konkat: "+konkat);
+        //testschleife sbox
+        int temp = 0;
+        for(int i=3; i<=31; i+=4) {
+            temp = sBox(i, temp, get4Bits(i, konkat), 0);
+        }
+        System.out.println(temp);
+        
+        int temp2 = 0;
+        for(int i=3; i<=31; i+=4) {
+            temp2 = invSBox(i, temp2, get4Bits(i, temp), 0);
+        }
+        System.out.println(temp2);
+        
+        
+//        int temp = sBox(31,concatValue,get4Bits(31, konkat),0);
+        
+        
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        
+//        System.out.println(i|a|b|c|d);
+//        System.out.println(set4Bits(31, i, 15));
+        
+//        System.out.println(i);
 //        System.out.println(set4Bits(3, i, 10));
         
+//        int i = 10212;
         
+//        -1 000 00000 00000 00000 00000 00000
         
 //        int [] in = new int[4];
 //        in[0] = 1242340;
