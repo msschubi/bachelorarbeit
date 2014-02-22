422ceb9888bb06bc00b26246fc8046e1a1d7b143
msschubi <m.schubert@tu-bs.de>
2014-02-22 22:46:00 +0100

    Fehlerkorrekturen

--- src/Twofish.java
+++ src/Twofish.java
@@ -6,6 +6,9 @@ public class Twofish {
     private static int[] S;
     private static int k;
 
+    private final static int p = 0x1010101; // 2^24+2^16+2^8+2^0
+    private static int[] K = new int[40];
+
     // q0, q1 Variablen
     private static int a0, a1, a2, a3, a4, b0, b1, b2, b3, b4;
 
@@ -13,50 +16,67 @@ public class Twofish {
     private static int y0, y1, y2, y3;
     private static int[] z = { 0, 0, 0, 0 };
 
+    public static void calcKey() {
+        int A, B,temp;
+        for (int i = 0; i < 20; i++) {
+            A = h(2 * i * p, Me);
+            B = h((2 * i + 1) * p, Mo);
+            B = (B << 8) | (B >>> 24);
+            // Pseudo-Hadamard Transformation
+            K[2 * i] = (int) ((A + B) % (0x100000000L)); // mod 2^32
+            temp = (int) ((A + 2 * B) % (0x100000000L));
+            K[2 * i + 1] = (temp << 9) | (temp >>> 23);
+        }
+    }
+
+//    public static int[] inputWhitening ()
     public static int g(int X) {
         return h(X, S);
     }
-
+    
     public static int h(int X, int[] L) {
-        y0 = X & 0xF;
-        y1 = (X >>> 8) & 0xF;
-        y2 = (X >>> 16) & 0xF;
-        y3 = (X >>> 24) & 0xF;
+        y0 = X & 0xFF;
+        y1 = (X >>> 8) & 0xFF;
+        y2 = (X >>> 16) & 0xFF;
+        y3 = (X >>> 24) & 0xFF;
+        
+//        System.out.println(y0+" "+y1+" "+y2+" "+y3);
 
         if (k == 4) {
-            y0 = q1(y0) ^ (L[3] & 0xF);
-            y1 = q0(y1) ^ ((L[3] >>> 8) & 0xF);
-            y2 = q0(y2) ^ ((L[3] >>> 16) & 0xF);
-            y3 = q1(y3) ^ ((L[3] >>> 24) & 0xF);
+            y0 = TFT.P[1][y0] ^ (L[3] & 0xFF);
+            y1 = TFT.P[0][y1] ^ ((L[3] >>> 8) & 0xFF);
+            y2 = TFT.P[0][y2] ^ ((L[3] >>> 16) & 0xFF);
+            y3 = TFT.P[1][y3] ^ ((L[3] >>> 24) & 0xFF);
         }
         if (k >= 3) {
-            y0 = q1(y0) ^ (L[2] & 0xF);
-            y1 = q1(y1) ^ ((L[2] >>> 8) & 0xF);
-            y2 = q0(y2) ^ ((L[2] >>> 16) & 0xF);
-            y3 = q0(y3) ^ ((L[2] >>> 24) & 0xF);
+            y0 = TFT.P[1][y0] ^ (L[2] & 0xFF);
+            y1 = TFT.P[1][y1] ^ ((L[2] >>> 8) & 0xFF);
+            y2 = TFT.P[0][y2] ^ ((L[2] >>> 16) & 0xFF);
+            y3 = TFT.P[0][y3] ^ ((L[2] >>> 24) & 0xFF);
+            System.out.println(y0+" "+y1+" "+y2+" "+y3);
         }
+        //TODO ersetzen durch Table
+        y0 = q1(q0(q0(y0) ^ (L[1] & 0xFF)) ^ (L[0] & 0xFF));
+        y1 = q0(q0(q1(y1) ^ ((L[1] >>> 8) & 0xFF)) ^ ((L[0] >>> 8) & 0xFF));
+        y2 = q1(q1(q0(y2) ^ ((L[1] >>> 16) & 0xFF)) ^ ((L[0] >>> 16) & 0xFF));
+        y3 = q0(q1(q1(y3) ^ ((L[1] >>> 24) & 0xFF)) ^ ((L[0] >>> 24) & 0xFF));
 
-        y0 = q1(q0(q0(y0) ^ (L[1] & 0xF)) ^ (L[0] & 0xF));
-        y1 = q0(q0(q1(y1) ^ ((L[1] >>> 8) & 0xF)) ^ ((L[0] >>> 8) & 0xF));
-        y2 = q1(q1(q0(y2) ^ ((L[1] >>> 16) & 0xF)) ^ ((L[0] >>> 16) & 0xF));
-        y3 = q0(q1(q1(y3) ^ ((L[1] >>> 24) & 0xF)) ^ ((L[0] >>> 24) & 0xF));
-
-        z[0] = Utils.multGF(TwofishTables.MDS[0][0], y0, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[0][1], y1, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[0][2], y2, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[0][3], y3, TwofishTables.IRRPOLYNOMTFMDS);
-        z[1] = Utils.multGF(TwofishTables.MDS[1][0], y0, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[1][1], y1, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[1][2], y2, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[1][3], y3, TwofishTables.IRRPOLYNOMTFMDS);
-        z[2] = Utils.multGF(TwofishTables.MDS[2][0], y0, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[2][1], y1, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[2][2], y2, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[2][3], y3, TwofishTables.IRRPOLYNOMTFMDS);
-        z[3] = Utils.multGF(TwofishTables.MDS[3][0], y0, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[3][1], y1, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[3][2], y2, TwofishTables.IRRPOLYNOMTFMDS) ^
-                Utils.multGF(TwofishTables.MDS[3][3], y3, TwofishTables.IRRPOLYNOMTFMDS);
+        z[0] = Utils.multGF(TFT.MDS[0][0], y0, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[0][1], y1, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[0][2], y2, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[0][3], y3, TFT.IRRPOLYNOMTFMDS);
+        z[1] = Utils.multGF(TFT.MDS[1][0], y0, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[1][1], y1, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[1][2], y2, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[1][3], y3, TFT.IRRPOLYNOMTFMDS);
+        z[2] = Utils.multGF(TFT.MDS[2][0], y0, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[2][1], y1, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[2][2], y2, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[2][3], y3, TFT.IRRPOLYNOMTFMDS);
+        z[3] = Utils.multGF(TFT.MDS[3][0], y0, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[3][1], y1, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[3][2], y2, TFT.IRRPOLYNOMTFMDS) ^
+                Utils.multGF(TFT.MDS[3][3], y3, TFT.IRRPOLYNOMTFMDS);
 
         return (z[3] << 24) | (z[2] << 16) | (z[1] << 8) | z[0];
     }
@@ -67,12 +87,12 @@ public class Twofish {
         a1 = a0 ^ b0;
         // Rotation um 1 von 4Bits
         b1 = (a0 ^ (((b0 << 1) & 0xF) | (b0 >> 3)) ^ (8 * a0)) % 16;
-        a2 = TwofishTables.sBoxQ0[0][a1];
-        b2 = TwofishTables.sBoxQ0[1][b1];
+        a2 = TFT.sBoxQ0[0][a1];
+        b2 = TFT.sBoxQ0[1][b1];
         a3 = a2 ^ b2;
         b3 = (a2 ^ (((b2 << 1) & 0xF) | (b2 >> 3)) ^ (8 * a2)) % 16;
-        a4 = TwofishTables.sBoxQ0[2][a3];
-        b4 = TwofishTables.sBoxQ0[3][b3];
+        a4 = TFT.sBoxQ0[2][a3];
+        b4 = TFT.sBoxQ0[3][b3];
 
         return 16 * b4 + a4;
     }
@@ -83,12 +103,12 @@ public class Twofish {
         a1 = a0 ^ b0;
         // Rotation um 1 von 4Bits
         b1 = (a0 ^ (((b0 << 1) & 0xF) | (b0 >> 3)) ^ (8 * a0)) % 16;
-        a2 = TwofishTables.sBoxQ1[0][a1];
-        b2 = TwofishTables.sBoxQ1[1][b1];
+        a2 = TFT.sBoxQ1[0][a1];
+        b2 = TFT.sBoxQ1[1][b1];
         a3 = a2 ^ b2;
         b3 = (a2 ^ (((b2 << 1) & 0xF) | (b2 >> 3)) ^ (8 * a2)) % 16;
-        a4 = TwofishTables.sBoxQ1[2][a3];
-        b4 = TwofishTables.sBoxQ1[3][b3];
+        a4 = TFT.sBoxQ1[2][a3];
+        b4 = TFT.sBoxQ1[3][b3];
 
         return 16 * b4 + a4;
     }
@@ -107,7 +127,7 @@ public class Twofish {
         }
         return padKey;
     }
-
+// Funktoniert
     public static void calcBasisKey(int[] M) {
         k = M.length / 2;
         Me = new int[k];
@@ -132,6 +152,7 @@ public class Twofish {
             Me[x] = Mi[2 * x];
             Mo[x] = Mi[2 * x + 1];
         }
+     
 
         // 8ter Byte Blöcke bilden aus dem eingegebenen Key M
         int m[][] = new int[k][8];
@@ -150,53 +171,54 @@ public class Twofish {
         for (int x = 0; x < k; x++) {
             // (k-1)-x, da woerter in umgekehrter reihenfolge laut beschreibung
             // in S sind
-            S[(k - 1) - x] = Utils.multGF(TwofishTables.RS[0][0], m[x][0], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[0][1], m[x][1], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[0][2], m[x][2], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[0][3], m[x][3], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[0][4], m[x][4], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[0][5], m[x][5], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[0][6], m[x][6], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[0][7], m[x][7], TwofishTables.IRRPOLYNOMTFKEY);
+            S[(k - 1) - x] = Utils.multGF(TFT.RS[0][0], m[x][0], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[0][1], m[x][1], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[0][2], m[x][2], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[0][3], m[x][3], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[0][4], m[x][4], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[0][5], m[x][5], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[0][6], m[x][6], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[0][7], m[x][7], TFT.IRRPOLYNOMTFKEY);
 
-            S[(k - 1) - x] |= (Utils.multGF(TwofishTables.RS[1][0], m[x][0], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[1][1], m[x][1], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[1][2], m[x][2], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[1][3], m[x][3], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[1][4], m[x][4], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[1][5], m[x][5], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[1][6], m[x][6], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[1][7], m[x][7], TwofishTables.IRRPOLYNOMTFKEY)) << 8;
+            S[(k - 1) - x] |= (Utils.multGF(TFT.RS[1][0], m[x][0], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[1][1], m[x][1], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[1][2], m[x][2], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[1][3], m[x][3], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[1][4], m[x][4], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[1][5], m[x][5], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[1][6], m[x][6], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[1][7], m[x][7], TFT.IRRPOLYNOMTFKEY)) << 8;
 
-            S[(k - 1) - x] |= (Utils.multGF(TwofishTables.RS[2][0], m[x][0], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[2][1], m[x][1], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[2][2], m[x][2], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[2][3], m[x][3], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[2][4], m[x][4], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[2][5], m[x][5], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[2][6], m[x][6], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[2][7], m[x][7], TwofishTables.IRRPOLYNOMTFKEY)) << 16;
+            S[(k - 1) - x] |= (Utils.multGF(TFT.RS[2][0], m[x][0], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[2][1], m[x][1], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[2][2], m[x][2], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[2][3], m[x][3], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[2][4], m[x][4], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[2][5], m[x][5], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[2][6], m[x][6], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[2][7], m[x][7], TFT.IRRPOLYNOMTFKEY)) << 16;
 
-            S[(k - 1) - x] |= (Utils.multGF(TwofishTables.RS[3][0], m[x][0], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[3][1], m[x][1], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[3][2], m[x][2], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[3][3], m[x][3], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[3][4], m[x][4], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[3][5], m[x][5], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[3][6], m[x][6], TwofishTables.IRRPOLYNOMTFKEY)
-                    ^ Utils.multGF(TwofishTables.RS[3][7], m[x][7], TwofishTables.IRRPOLYNOMTFKEY)) << 24;
+            S[(k - 1) - x] |= (Utils.multGF(TFT.RS[3][0], m[x][0], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[3][1], m[x][1], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[3][2], m[x][2], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[3][3], m[x][3], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[3][4], m[x][4], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[3][5], m[x][5], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[3][6], m[x][6], TFT.IRRPOLYNOMTFKEY)
+                    ^ Utils.multGF(TFT.RS[3][7], m[x][7], TFT.IRRPOLYNOMTFKEY)) << 24;
         }
     }
 
     public static void main(String[] args) {
         int p = 87;
         int q = 19;
-        int M[] = { 1, 2, 3, 4, 1, 2, 3, 4 };
+        int M[] = { 1,0,0,0,0,0,1,2 };
         calcBasisKey(paddingKey(M));
+        
+        calcKey();
 
-        int a = 233822232;
-        int b;
-
-        System.out.println(h(a, S));
+//        for (int k : K) {
+//            System.out.println(k);
+//        }
     }
 }
